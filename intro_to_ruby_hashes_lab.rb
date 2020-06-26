7eca392d40c1f2b33131932cc0e043eb3d184116
Cameron Hudson <camnn3@yahoo.com>
2020-06-26 02:37:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -52,7 +52,8 @@ def monopoly_with_fourth_tier
 	    :b_and_o_railroad => {"mortgage_value"=>"$400"},
 	    :shortline => {"mortgage_value"=>"$800"}
 	    },
-	    :rent_in_dollars => {
+	    :rent_in_dollars => 
+	    {
 	      :one_piece_owned => 25,
         :two_pieces_owned => 50,
         :three_pieces_owned => 100,
